0086ad1e0b33dcdaad2e8389e59ef14e8bd19084
Frank McOpare <frank.mcopare@yahoo.com>
2024-01-31 19:43:49 +0000

    declare variables in javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+let questions = [];
